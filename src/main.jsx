97261b6b1d9c0f08fc5140c2f958357e25a3ab41
prashanth1406 <62340564+prashanth1406@users.noreply.github.com>
2026-07-13 13:36:25 +0000

    Fixed Tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './components/Login/Login.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
