173feae76fa0d007863bbfc070c23733e55b6d55
Prashanth <prashanth.bussa@octanner.com>
2026-07-21 21:33:20 +0530

    AA-8: Create New Route for Home Page
    Generated by dev agent.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login.jsx';
+import Home from './components/Home.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
